7660c3476a2241fee49dcdd1bdb585774bf7d5c0
Trebol95 <96895419+Trebol95@users.noreply.github.com>
2023-08-24 23:48:44 +0200

    Add files via upload

---
+++ project1/index.js
@@ -0,0 +1,36 @@
+const hourEl = document.getElementById("hour")
+const minutesEL = document.getElementById("minutes")
+const secondsEL = document.getElementById("seconds")
+const ampmEL = document.getElementById("ampm")
+
+function updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h > 12) {
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h<10 ? "0" + h : h;
+    m = m<10 ? "0" + m : m;
+    s = s<10 ? "0" + s : s;
+
+
+
+
+    hourEl.innerText = h;
+    minutesEL.innerText = m;
+    secondsEL.innerText = s;
+    ampmEL,innerText = ampm;
+    setTimeout(() =>{
+
+        updateClock()
+    }, 1000)
+
+} 
+
+
+updateClock()
